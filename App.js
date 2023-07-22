bd83ba86128396653ca51870ddc48d660c9046f6
Adam Sinning <asinning25@gmail.com>
2023-07-22 16:36:02 -0600

    Added camera functionality

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasCameraPermission === null) {
+    return <View />;
+  }
+  if (hasCameraPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+ 
+  const handleCameraTypeChange = () => {
+    setType(
+      type === Camera.Constants.Type.back
+        ? Camera.Constants.Type.front
+        : Camera.Constants.Type.back
+    );
+  };
+
+  const handleImagePick = async () => {
+    const image = await ImagePicker.launchImageLibraryAsync();
+    if (!image.canceled) {
+      // Handle the selected image
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Adam</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <Camera style={{ flex: 1 }} type={type}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: 'transparent',
+            flexDirection: 'row',
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              flex: 0.1,
+              alignSelf: 'flex-end',
+              alignItems: 'center',
+            }}
+            onPress={handleCameraTypeChange}
+          >
+            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+              Flip
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+      <TouchableOpacity
+        style={{
+          backgroundColor: 'blue',
+          padding: 10,
+          margin: 20,
+          borderRadius: 5,
+        }}
+        onPress={handleImagePick}
+      >
+        <Text style={{ color: 'white', fontSize: 18 }}>Pick Image</Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
